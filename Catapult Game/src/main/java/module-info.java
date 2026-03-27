169e33dbdf04862b095addadb56edc132ba6fcf0
K1d_Named_F1nger <143749881+Fireice2@users.noreply.github.com>
2026-03-27 09:48:47 -0700

    Delete Catapult Game directory

--- Catapult Game/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.catapult_game {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.catapult_game to javafx.fxml;
-    exports com.example.catapult_game;
-}
